8ac4b75c3d18f96f589ed4c822c7c966ae9a10c6
justkost <ostrouhovk@yandex.ru>
2017-08-13 22:11:20 +0700

    + panel style

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+const styles = {
+  panel: {
+    display: 'table',
+    borderCollapse: 'collapse'
+  },
+  btn: {
+    display: 'table-cell',
+    padding: '5px 10px',
+    textDecoration: 'none',
+    color: '#000',
+    border: '1px solid #000',
+    fontSize: '14px'
+  }
+}
+
 class Panel extends Component {
+  onClick = (type) => (e) => {
+    e.preventDefault()
+    console.log(type)
+  }
+
   render () {
     return (
-      <div>
-        <button>Bold</button>
-        <button>List</button>
+      <div style={ styles.panel }>
+        <a href="#" onClick={ this.onClick('bold') } style={ styles.btn }>
+          Bold
+        </a>
+        <a href="#" onClick={ this.onClick('list') } style={ styles.btn }>
+          List
+        </a>
       </div>
     )
   }
